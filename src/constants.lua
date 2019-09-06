cc02f7b08e0a3eff2d39acc95e0bc3efb19472a2
leesharped <leesharped@gmail.com>
2019-09-06 08:35:28 +0800

    Initial commit

---
+++ src/constants.lua
@@ -0,0 +1,24 @@
+--[[
+    GD50 2018
+    Breakout Remake
+
+    -- constants --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Some global constants for our application.
+]]
+
+-- size of our actual window
+arenaWidth= 800
+arenaHeight = 600
+
+-- size we're trying to emulate with push
+VIRTUAL_WIDTH = 800
+VIRTUAL_HEIGHT = 600
+
+
+
+-- paddle movement speed
+PADDLE_SPEED = 200
